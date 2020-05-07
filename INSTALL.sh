b6dd54e8abc2c10fe7b0ea189da0ff75c0d133ed
Dindin Hernawan <dindin_hernawan@yahoo.com>
2020-05-08 00:09:22 +0800

    Upload Linux-Dark18

---
+++ INSTALL.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd icons
+sudo cp Linux-Dark /usr/share/icons/
+cd ..
+cd themes
+sudo cp Linux-Dark /usr/share/themes/
+echo '===================================='
+echo ' OK DONE INSTALL SUKSES '
+echo '===================================='
